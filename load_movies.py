a7da99b15ed0420151fcb9f8df77e00512b396e3
Vinay Mishra <129633599+Vinay1890@users.noreply.github.com>
2024-07-11 11:17:02 +0530

    Update load_movies.py

--- load_movies.py
+++ load_movies.py
@@ -2,7 +2,7 @@ import json
 from pymongo import MongoClient
 
 # Connection string to your MongoDB (or CosmosDB) instance
-connection_string = "mongodb://movies-db-account:Oy9jia97GXUEsBpAlggXaHJiPqrlCqNeQ4lvxJxROLpw1EQWVR3tw9D4uQ9b4JTAprf6XhEhih14ACDbVQNb9w==@movies-db-account.mongo.cosmos.azure.com:10255/?ssl=true&replicaSet=globaldb&retrywrites=false&maxIdleTimeMS=120000&appName=@movies-db-account@"
+connection_string = "your_cosmos_db_connection_string"
 
 # Connect to the MongoDB cluster
 client = MongoClient(connection_string)
